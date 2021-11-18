6a2d465b909a6bd7a2a1ed656ac964e9c75eb5ff
Renata Leal <85504267+RenatadsLeal@users.noreply.github.com>
2021-11-18 11:10:04 -0300

    criando banana project

---
+++ src/main/java/com/renata/bananajenkins/BananajenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.renata.bananajenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananajenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananajenkinsApplication.class, args);
+	}
+
+}
